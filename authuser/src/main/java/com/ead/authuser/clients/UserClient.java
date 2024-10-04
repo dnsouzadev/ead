406df5a1131c4a96f2b291e35e34550e60323943
Daniel Souza <workdndsza@gmail.com>
2024-10-04 19:55:29 -0300

    retornando os cursos de um usuario retornando dados de outro microservice

--- authuser/src/main/java/com/ead/authuser/clients/UserClient.java
+++ authuser/src/main/java/com/ead/authuser/clients/UserClient.java
@@ -1,10 +1,15 @@
 package com.ead.authuser.clients;
 
 import com.ead.authuser.dtos.CourseDto;
+import com.ead.authuser.dtos.ResponsePageDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,19 +27,22 @@ public class UserClient {
 
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable) {
         List<CourseDto> searchResult = null;
-        String url = REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size=" + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(":", ",");
+        String url = REQUEST_URI + "/courses?userId=" + userId + "&page=" + pageable.getPageNumber() + "&size=" + pageable.getPageSize() + "&sort=id".replaceAll(":", ",");
 
         log.debug("Request URL: {}", url);
         log.info("Request URL: {}", url);
 
         try {
+            ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+            ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+            searchResult = result.getBody().getContent();
             log.debug("Response Number of Elements: {}", searchResult.size());
-            searchResult = restTemplate.getForObject(url, List.class);
         } catch (Exception e) {
             log.error("Error: {}", e.getMessage());
             e.printStackTrace();
         }
 
         log.info("Ending request /courses userId {}", userId);
-        return ;
+        return new PageImpl<>(searchResult);
     }
+}
