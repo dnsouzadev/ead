93c1d57c573d26ecf1b2c991ad703b58d78158bb
Daniel Souza <workdndsza@gmail.com>
2024-10-29 09:09:22 -0300

    preparando asynchronous comunication via commands

---
+++ notification/src/main/java/com/ead/notification/models/NotificationModel.java
@@ -0,0 +1,37 @@
+package com.ead.notification.models;
+
+import com.ead.notification.enums.NotificationStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "TB_NOTIFICATIONS")
+@Getter
+@Setter
+public class NotificationModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID notificationId;
+    @Column(nullable = false)
+    private UUID userId;
+    @Column(nullable = false, length = 150)
+    private String title;
+    @Column(nullable = false)
+    private String message;
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus notificationStatus;
+}
